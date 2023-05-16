fc64b0d17251f17cfbc0b5a22f58744238772db1
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-05-16 15:57:23 -0500

    clean app

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={inter.className}>{children}</body>
     </html>
   )
