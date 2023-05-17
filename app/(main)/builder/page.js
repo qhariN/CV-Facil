bc83eff4fec403c8beb4c93ae0aa16b7de44191d
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-05-17 18:31:33 -0500

    multiple columns one div test

--- app/(main)/builder/page.js
+++ app/(main)/builder/page.js
@@ -13,7 +13,11 @@ export default function Builder() {
         </div>
       </div>
       <div className="grow">
-        <Test />
+        <div className='min-h-[300px] max-h-[300px] columns-[auto_140px] gap-3'>
+          <div className='bg-white text-black break-inside-avoid'>
+            <Test title='test prop' />
+          </div>
+        </div>
       </div>
     </>
   )
