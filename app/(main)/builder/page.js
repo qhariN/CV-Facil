77ebe9a796e3553d0bfce4b56b012302325b511b
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-05-18 17:22:47 -0500

    add pdfmake

--- app/(main)/builder/page.js
+++ app/(main)/builder/page.js
@@ -1,7 +1,21 @@
+'use client'
 import FormHeader from '@/components/builder/form/form-header'
 import Test from './test.mdx'
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export default function Builder() {
+  const jspdf = () => {
+    const dd = {
+      content: [
+        'First paragraph',
+        'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+      ]
+    }
+    pdfMake.createPdf(dd).open()
+  }
+
   return (
     <>
       <div className="grow">
@@ -9,6 +23,7 @@ export default function Builder() {
         <div className='h-full overflow-y-auto'>
           <div className='min-h-full h-[2000px]'>
             form
+            <button onClick={jspdf}>click</button>
           </div>
         </div>
       </div>
